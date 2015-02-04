aebd83daf1cab7f9e5e0bb1f18b54db6e2d4f4b6
Chino <Israel@Israels-MacBook-Air.local>
2015-02-04 18:19:03 -0500

    I removed the button hide function

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
-$('.footer-nav').hide();
+/*$('.footer-nav').hide();
 $(window).scroll(function() {
 	if($(window).scrollTop() + $(window).height() == $(document).height()) {
 		$(".footer-nav").show();
 	}
 });
+*/
