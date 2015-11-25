d5ba1a49374160ee194297486d5e5ec6509767b4
Israel <israel@Israels-MacBook-Air.local>
2015-11-24 21:22:10 -0500

    second

--- js/main.js
+++ js/main.js
@@ -1,22 +1,15 @@
-// $(document).ready(function(){
+$(document).ready(function(){
 
-// 	// stores all the images found in the img folder
-// 	var img_name = ['building.jpeg', 'firework-sky.jpeg', 'fireworks.jpeg', 'mountain.jpeg', 'road.jpeg', 'sky.jpeg', 'work-stick.jpeg', 'work.jpeg'];
+	// stores all the images found in the img folder
+	var img_name = ['building.jpeg', 'firework-sky.jpeg', 'fireworks.jpeg', 'mountain.jpeg', 'road.jpeg', 'sky.jpeg', 'work-stick.jpeg', 'work.jpeg'];
 
-<<<<<<< HEAD
 	// this loop appends each image in the gallery division
 	for(var i = 0; i < img_name.length; i++){
 		$('#gallery').append('<a href="./img/'+img_name[i]+'" data-lightbox="gallery"><div class="image" style="background: url(./img/'+img_name[i]+');"></div></a>');
 	}
-=======
-// 	// this loop appends each image in the gallery division
-// 	for(var i = 0; i < img_name.length; i++){
-// 		$('#gallery').append('<div class="image" style="background: url(./img/'+img_name[i]+'); data-lightbox=\'gallery\'"></div>');
-// 	}
->>>>>>> master
 
-// 	// this styles the images so they would fit perfectly on a square
-// 	$('.image').css('background-position', 'center'); 
-// 	$('.image').css('background-size', 'cover'); 
+	// this styles the images so they would fit perfectly on a square
+	$('.image').css('background-position', 'center'); 
+	$('.image').css('background-size', 'cover'); 
 
-// });
+});
