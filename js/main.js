05e1c32f731b928941f19cad793a774c9e66c258
Israel <israel@Israels-MacBook-Air.local>
2015-11-24 20:30:28 -0500

    added lightbox

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
-$(document).ready(function(){
+// $(document).ready(function(){
 
-	// stores all the images found in the img folder
-	var img_name = ['building.jpeg', 'firework-sky.jpeg', 'fireworks.jpeg', 'mountain.jpeg', 'road.jpeg', 'sky.jpeg', 'work-stick.jpeg', 'work.jpeg'];
+// 	// stores all the images found in the img folder
+// 	var img_name = ['building.jpeg', 'firework-sky.jpeg', 'fireworks.jpeg', 'mountain.jpeg', 'road.jpeg', 'sky.jpeg', 'work-stick.jpeg', 'work.jpeg'];
 
-	// this loop appends each image in the gallery division
-	for(var i = 0; i < img_name.length; i++){
-		$('#gallery').append('<div class="image" style="background: url(./img/'+img_name[i]+');"></div>');
-	}
+// 	// this loop appends each image in the gallery division
+// 	for(var i = 0; i < img_name.length; i++){
+// 		$('#gallery').append('<div class="image" style="background: url(./img/'+img_name[i]+'); data-lightbox=\'gallery\'"></div>');
+// 	}
 
-	// this styles the images so they would fit perfectly on a square
-	$('.image').css('background-position', 'center'); 
-	$('.image').css('background-size', 'cover'); 
+// 	// this styles the images so they would fit perfectly on a square
+// 	$('.image').css('background-position', 'center'); 
+// 	$('.image').css('background-size', 'cover'); 
 
-});
+// });
