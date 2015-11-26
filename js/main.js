f7f4fb4d02f7f1435629fb383b6f69474c66813a
Israel <israel@Israels-MacBook-Air.local>
2015-11-25 21:58:56 -0500

    fixed the about page

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
 	var img_name = ['building.jpeg', 'firework-sky.jpeg', 'fireworks.jpeg', 'mountain.jpeg', 'road.jpeg', 'sky.jpeg', 'work-stick.jpeg', 'work.jpeg'];
 
 	// this loop appends each image in the gallery division
+	// this will also adds an anchor element to the doc
 	for(var i = 0; i < img_name.length; i++){
 		$('#gallery').append('<a href="./img/'+img_name[i]+'" data-lightbox="gallery"><div class="image" style="background: url(./img/'+img_name[i]+');"></div></a>');
 	}
